e284c1846abc9e2b3863fc9c406b2dc3d9de4aef
Lê Văn Lâm <87559387+LeeLam2001@users.noreply.github.com>
2021-07-17 13:34:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const CONFIG = {
     btnIntro: 'Ấn vô tiếp đi ^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    btnYes: 'Vẫn cứ là yêu anh <33',
     btnNo: 'Không chọn được cái này đâu lew lew ^^ :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
