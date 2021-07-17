941166fa621c2c0df32015fdecb4690f38fb6385
Lê Văn Lâm <87559387+LeeLam2001@users.noreply.github.com>
2021-07-17 13:31:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
     introTitle: 'Bae à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    introDesc: `Trái đất vốn lạ thường,
+    Mà sao em cứ đi nhầm đường,
+    Lạc vào tim anh lẻ loi,
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    btnIntro: 'Ấn vô tiếp đi ^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích anh <33',
     btnNo: 'Không chọn được cái này đâu lew lew ^^ :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Bởi vì em yêu anh <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Yêu em nhiều lắm nhó <333.',
     btnAccept: 'Okiiiii lun <3',
