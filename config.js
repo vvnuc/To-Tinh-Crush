6c71e944a4f8125715fc6952ebab7b3ee56c92a6
Namhackertretrau <91825933+Namhackertretrau@users.noreply.github.com>
2021-10-21 00:28:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Phượng à, 5 hên đây :>',
+    introTitle: 'Thương à, anh Nam đây :>',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -7,7 +7,7 @@ const CONFIG = {
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: '8 thích anh <33',
+    btnYes: 'Anh thích em <33',
     btnNo: 'Không, 8 coi 5 là a trai :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao 8 lại yêu 5 <3',
     btnReply: 'Gửi cho lão 5 hên <3',
