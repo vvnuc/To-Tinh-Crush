e8a2a8caabc042418f6337f8d18d58155b668f23
Lê Văn Lâm <87559387+LeeLam2001@users.noreply.github.com>
2022-12-03 12:42:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Yêu em nhiều lắm nhó <33.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://m.me/lee04092021' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://m.me/lee04092021'
 }
